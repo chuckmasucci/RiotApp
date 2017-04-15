d3f1e9053ec8efd8a21726cc24040957fa2b9b9a
Chuck Masucci <cmasucci@gmail.com>
2017-04-15 10:50:04 -0400

    Refactored API request into single function

--- app/src/Models/RiotModel.php
+++ app/src/Models/RiotModel.php
@@ -17,58 +17,55 @@ class RiotModel
         $this->api_key = "RGAPI-563dfbac-aa37-4bb9-a3a1-d54c8b9ea1c2";
         $this->summoner_id = "35301382";
 
-        $match_data = $this->getMatchAPI();
+        // Summoner Spells
+        $spell_data = $this->makeApiRequest(
+            $base_url="https://global.api.riotgames.com/api/lol/static-data/NA/",
+            $version = "v1.2",
+            $endpoint = "/summoner-spell",
+            $params = "&spellData=all"
+        );
+        $this->updateSummonerSpellDB($spell_data->data);
 
-        echo "<pre>";
-        var_dump($match_data);
-        echo "</pre>";
+        // Items
+        $item_data = $this->makeApiRequest(
+            $base_url = "https://global.api.riotgames.com/api/lol/static-data/NA/",
+            $version = "v1.2",
+            $endpoint = "/item"
+        );
+        $this->updateItemsListDB($item_data->data);
 
-        // $champion_data = $champion_data = $this->getChampionsAPI();
-        // $this->updateChampionListDB($champion_data);
+        // Champions
+        $champion_data = $this->makeApiRequest(
+            $base_url = "https://global.api.riotgames.com/api/lol/static-data/NA/",
+            $version = "v1.2",
+            $endpoint = "/champion"
+        );
+        $this->updateChampionListDB($champion_data->data);
 
-        // Get match data from Riot API
-        // $match_list_data = $this->getMatchListAPI();
+        // Champions
+        $match_list_data = $this->makeApiRequest(
+            $base_url = "https://na.api.riotgames.com/api/lol/NA/",
+            $version = "v2.2",
+            $endpoint = "/matchlist",
+            $request_type = "/by-summoner/" . $this->summoner_id,
+            $params = "&beginIndex=0&endIndex=25"
+        );
+        $this->updateMatchListDB($match_list_data->matches);
 
-        // Update the database with the API data
-        // $this->updateMatchListDB($match_list_data);
+
+        // $match_data = $this->getMatchAPI();
     }
 
-    private function getChampionsAPI()
+    private function makeApiRequest($base_url = NULL, $version = NULL, $endpoint = NULL, $request_type = NULL, $params = NULL)
     {
-        $base_url = "https://global.api.riotgames.com/api/lol/static-data/NA/";
-        $version = "v1.2";
-        $endpoint = "/champion";
-
         $curl = curl_init();
         curl_setopt_array($curl, [
             CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $base_url . $version . $endpoint . "?api_key=" . $this->api_key
+            CURLOPT_URL => $base_url . $version . $endpoint . $request_type . "?api_key=" . $this->api_key . $params
         ]);
 
         $resp = curl_exec($curl);
-        $resp = json_decode($resp)->data;
-        curl_close($curl);
-
-        return $resp;
-
-    }
-
-    private function getMatchListAPI()
-    {
-        $base_url = "https://na.api.riotgames.com/api/lol/NA/";
-        $version = "v2.2";
-        $endpoint = "/matchlist";
-        $request_type = "/by-summoner";
-        $params = "&beginIndex=0&endIndex=25";
-
-        $curl = curl_init();
-        curl_setopt_array($curl, [
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $base_url . $version . $endpoint . $request_type . "/" . $this->summoner_id . "?api_key=" . $this->api_key . $params
-        ]);
-
-        $resp = curl_exec($curl);
-        $resp = json_decode($resp)->matches;
+        $resp = json_decode($resp);
         curl_close($curl);
 
         return $resp;
@@ -76,7 +73,6 @@ class RiotModel
 
     private function getMatchAPI($match_id = 2473292032)
     {
-        // https://na.api.riotgames.com/api/lol/NA/v2.2/match/2473292032?api_key=RGAPI-563dfbac-aa37-4bb9-a3a1-d54c8b9ea1c2
         $base_url = "https://na.api.riotgames.com/api/lol/NA/";
         $version = "v2.2";
         $endpoint = "/match";
@@ -95,11 +91,54 @@ class RiotModel
         return $resp;
     }
 
+    private function updateSummonerSpellDB($spell_data)
+    {
+        $stmt = $this->db->prepare("INSERT INTO summoner_spell (spellId, name, description) VALUES (:spellId, :name, :description)");
+        $stmt->bindParam(':spellId', $spellId);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':description', $description);
+
+        foreach ($spell_data as $value) {
+            $spellId = $value->id;
+            if (isset($value->name)) {
+                $name = $value->name;
+            }
+            if (isset($value->description)) {
+                $description = $value->description;
+            }
+
+            $stmt->execute();
+        }
+    }
+
+    private function updateItemsListDB($item_data)
+    {
+        $stmt = $this->db->prepare("INSERT INTO items (itemId, name, description, plaintext) VALUES (:itemId, :name, :description, :plaintext)");
+        $stmt->bindParam(':itemId', $itemId);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':plaintext', $plaintext);
+
+        foreach ($item_data as $value) {
+            $itemId = $value->id;
+            if (isset($value->name)) {
+                $name = $value->name;
+            }
+            if (isset($value->description)) {
+                $description = $value->description;
+            }
+            if (isset($value->plaintext)) {
+                $plaintext = $value->plaintext;
+            }
+
+            $stmt->execute();
+        }
+    }
+
     private function updateChampionListDB($match_data)
     {
         $stmt = $this->db->prepare("INSERT INTO champions (championId, name) VALUES (:championId, :name)");
         $stmt->bindParam(':championId', $championId);
-        // $stmt->bindParam(':key', $key);
         $stmt->bindParam(':name', $name);
 
         foreach ($match_data as $value) {
